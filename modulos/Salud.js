a7be8e4862f51ddeed0e304e4b99b1bb6483652e
keyner <keinerf588@gmail.com>
2025-11-13 20:41:47 -0500

    Salud.js: define y exporta la función SaludoPersonalizado que genera un mensaje de bienvenida con el nombre recibido

---
+++ modulos/Salud.js
@@ -0,0 +1,4 @@
+// Exportamos la funcion Saludopersonalizado para que pueda usarse en otros archivos
+export function SaludoPersonalizado(nombre) { // Define la funcion y recibe el nombre
+    return "Bienvenido " + nombre; // Retorna el saludo concatenado con el nombre
+}
